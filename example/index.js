b90e06bf802b0f472b497c3aba16fd9e914d143e
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2021-07-12 20:55:09 +0530

    Updated the example

--- example/index.js
+++ example/index.js
@@ -1,15 +1,17 @@
-import "regenerator-runtime/runtime";
-
 import { createClient } from "../lib/index";
 
 // Create A Client
 const client = createClient("http://localhost:2345/func");
 
 // Create a Callable
-const myServerFunc = client.callable("hello");
-const getUsers = client.callable("users.getAll");
+const myServerFunc = client.callable("functionName");
 
 // Call The function
-getUsers().then((res) => {
+myServerFunc(/* Pass the arguements here */).then((res) => {
+  console.log(res);
+});
+
+// or Directly call the function
+client.call("functionName" /* Pass the arguements here */).then((res) => {
   console.log(res);
 });
